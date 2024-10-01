9b9976113c1fff20998045c4536a778f76bc3143
FaridHajiyev3 <faridhajiev3@gmail.com>
2024-10-01 13:00:46 +0400

    first commit

---
+++ src/components/Page2/page2.js
@@ -0,0 +1,14 @@
+const faPlus = document.querySelectorAll(".plus");
+const itemNav = document.querySelectorAll(".item-nav");
+
+faPlus.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        const itemNavIndex = itemNav[index];
+
+        if (itemNavIndex.style.height === "0px" || !itemNavIndex.style.height) {
+            itemNavIndex.style.height = itemNavIndex.scrollHeight + "px"; // Açılan hündürlük
+        } else {
+            itemNavIndex.style.height = "0px"; // Bağlanan hündürlük
+        }
+    });
+});
